db5377aaf33ed8031a41576880cfcbf276a7268e
qingshan412 <qingshan412@hotmail.com>
2020-04-12 18:01:34 -0400

    Update align_images.py

--- align_images.py
+++ align_images.py
@@ -15,6 +15,13 @@ def unpack_bz2(src_path):
         fp.write(data)
     return dst_path
 
+def align_file(src, tgt):
+    for img_name in os.listdir(src):
+        print('processing', img_name)
+        raw_img_path = os.path.join(src, img_name)
+        face_landmarks = landmarks_detector.get_landmarks(raw_img_path)[0]
+        aligned_face_path = os.path.join(tgt, img_name)
+        image_align(raw_img_path, aligned_face_path, face_landmarks)
 
 if __name__ == "__main__":
     """
@@ -32,10 +39,18 @@ if __name__ == "__main__":
     os.makedirs(ALIGNED_IMAGES_DIR, exist_ok=True)
 
     landmarks_detector = LandmarksDetector(landmarks_model_path)
-    for img_name in os.listdir(RAW_IMAGES_DIR):
-        raw_img_path = os.path.join(RAW_IMAGES_DIR, img_name)
-        for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(raw_img_path), start=1):
-            face_img_name = '%s_%02d.png' % (os.path.splitext(img_name)[0], i)
-            aligned_face_path = os.path.join(ALIGNED_IMAGES_DIR, face_img_name)
 
-            image_align(raw_img_path, aligned_face_path, face_landmarks)
+    if 'divided' in RAW_IMAGES_DIR:
+        for folder_name in os.listdir(RAW_IMAGES_DIR):
+            os.makedirs(ALIGNED_IMAGES_DIR + os.sep + folder_name, exist_ok=True)
+            print('processing', folder_name)
+                for img_name in os.listdir(RAW_IMAGES_DIR + os.sep + folder_name):
+                    align_file(RAW_IMAGES_DIR + os.sep + folder_name, ALIGNED_IMAGES_DIR + os.sep + folder_name)
+    else:
+        align_file(RAW_IMAGES_DIR, ALIGNED_IMAGES_DIR)
+
+        # for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(raw_img_path), start=1):
+        #     face_img_name = '%s_%02d.png' % (os.path.splitext(img_name)[0], i)
+        #     aligned_face_path = os.path.join(ALIGNED_IMAGES_DIR, face_img_name)
+
+        #     image_align(raw_img_path, aligned_face_path, face_landmarks)
