b238339fce631ba2f48644edb51219ca00237d10
qingshan412 <qingshan412@hotmail.com>
2020-04-12 22:56:34 -0400

    Update latent2face.py

--- latent2face.py
+++ latent2face.py
@@ -31,43 +31,66 @@ def move_and_save(latent_vector, direction, coeffs, path):
 # # load the pre-trained generator
 # URL_FFHQ = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'
 
-tflib.init_tf()
-# with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='for face transformation')
+    parser.add_argument("-n", "--npy_dir", help="where to get embeded numpy", 
+                        default="cache/dist/rcs_256_st_1024", type=str)
+    parser.add_argument('-s','--save_dir',help='where to save generated images',default="cache/dist/fake", type=str)
+    parser.add_argument("-g", "--use_gpu", help="whether to use gpu or not", action="store_true")
+    
+    args = parser.parse_args()
 
-with open('cache/karras2019stylegan-ffhq-1024x1024.pkl', 'rb') as f:
-    generator_network, discriminator_network, Gs_network = pickle.load(f)
-print('model loaded.')
+    directions = ['smile', 'gender', 'age']
 
-generator = Generator(Gs_network, batch_size=1, randomize_noise=False)
-print('generator ready.')
+    for dire in directions:
+        os.makedirs(args.save_dir + os.sep + dire, exist_ok=True)
 
-# Loading already learned representations
-donald_trump = np.load('ffhq_dataset/latent_representations/donald_trump_01.npy')
-hillary_clinton = np.load('ffhq_dataset/latent_representations/hillary_clinton_01.npy')
-print('faces loaded.')
+    tflib.init_tf()
+    # with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
 
-# Of course you can learn your own vectors using two scripts
+    with open('cache/karras2019stylegan-ffhq-1024x1024.pkl', 'rb') as f:
+        generator_network, discriminator_network, Gs_network = pickle.load(f)
+    print('model loaded.')
 
-# 1) Extract and align faces from images
-# python align_images.py raw_images/ aligned_images/
+    generator = Generator(Gs_network, batch_size=1, randomize_noise=False)
+    print('generator ready.')
 
-# 2) Find latent representation of aligned images
-# python encode_images.py aligned_images/ generated_images/ latent_representations/
+    if 'divi' in args.npy_dir:
+        pass 
+    else:
+        img_files = os.listdir(args.npy_dir)
+        for img_file in img_files:
+            img_npy = np.load(args.npy_dir + os.sep + img_file)
+            for dire in directions:
+                move_and_save(img_npy, smile_direction, [-1.5, 0, 2], os.path.join(args.save_dir, dire, img_file)
 
-# Loading already learned latent directions
-smile_direction = np.load('ffhq_dataset/latent_directions/smile.npy')
-gender_direction = np.load('ffhq_dataset/latent_directions/gender.npy')
-age_direction = np.load('ffhq_dataset/latent_directions/age.npy')
-print('learned representations loaded.')
+    # # # Loading already learned representations
+    # # donald_trump = np.load('ffhq_dataset/latent_representations/donald_trump_01.npy')
+    # # hillary_clinton = np.load('ffhq_dataset/latent_representations/hillary_clinton_01.npy')
+    # # print('faces loaded.')
 
-move_and_save(donald_trump, smile_direction, [-1, 0, 2], 'recs/init/simile_trump.png')
-move_and_save(hillary_clinton, smile_direction, [-1, 0, 1], 'recs/init/simile_hilary.png')
-print('smiling images generated.')
+    # # Of course you can learn your own vectors using two scripts
 
-move_and_save(donald_trump, gender_direction, [-2, 0, 2], 'recs/init/gender_trump.png')
-move_and_save(hillary_clinton, gender_direction, [-1.5, 0, 1.], 'recs/init/gender_hilary.png')
-print('gender inverse images generated.')
+    # # 1) Extract and align faces from images
+    # # python align_images.py raw_images/ aligned_images/
 
-move_and_save(donald_trump, age_direction, [-2, 0, 2], 'recs/init/age_trump.png')
-move_and_save(hillary_clinton, age_direction, [-2, 0, 2], 'recs/init/age_hilary.png')
-print('aged images generated.')
+    # # 2) Find latent representation of aligned images
+    # # python encode_images.py aligned_images/ generated_images/ latent_representations/
+
+    # # Loading already learned latent directions
+    # smile_direction = np.load('ffhq_dataset/latent_directions/smile.npy')
+    # gender_direction = np.load('ffhq_dataset/latent_directions/gender.npy')
+    # age_direction = np.load('ffhq_dataset/latent_directions/age.npy')
+    # print('learned representations loaded.')
+
+    # move_and_save(donald_trump, smile_direction, [-1, 0, 2], 'recs/init/simile_trump.png')
+    # move_and_save(hillary_clinton, smile_direction, [-1, 0, 1], 'recs/init/simile_hilary.png')
+    # print('smiling images generated.')
+
+    # move_and_save(donald_trump, gender_direction, [-2, 0, 2], 'recs/init/gender_trump.png')
+    # move_and_save(hillary_clinton, gender_direction, [-1.5, 0, 1.], 'recs/init/gender_hilary.png')
+    # print('gender inverse images generated.')
+
+    # move_and_save(donald_trump, age_direction, [-2, 0, 2], 'recs/init/age_trump.png')
+    # move_and_save(hillary_clinton, age_direction, [-2, 0, 2], 'recs/init/age_hilary.png')
+    # print('aged images generated.')
