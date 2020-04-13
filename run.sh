a43077eaf521dfdcd1c6d2f560b56555cb965dd0
qingshan412 <qingshan412@hotmail.com>
2020-04-12 22:15:50 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@
 #$ -pe smp 16            # Specify parallel environment and legal core size
 #$ -q gpu
 #$ -l gpu_card=1
-#$ -N verify_test_gpu         # Specify job name
+#$ -N encode_test_gpu         # Specify job name
 
-python align_images.py cache/dist/orig cache/dist/st_1024
+module load tensorflow/1.12
+
+python encode_images.py cache/dist/st_1024 cache/dist/rcs_1024_st_1024 cache/dist/latent_st_1024_1024 --image_size=1024
+python encode_images.py cache/dist/st_1024 cache/dist/rcs_256_st_1024_b10 cache/dist/latent_st_1024_b10 --batch_size=10
+# python encode_images.py cache/dist/st_1024 cache/dist/rcs_256_st_1024 cache/dist/latent_st_1024
+# python align_images.py cache/dist/orig cache/dist/st_1024
+# st: set (size), rcs: reconstructed, 
