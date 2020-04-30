68176d0d149a895b828cc5e5444aae1d4fcddfb2
qingshan412 <qingshan412@hotmail.com>
2020-04-29 22:32:59 -0400

    Update latent2face.py

--- latent2face.py
+++ latent2face.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 
     for i in range(len(names_noonan)):
         for j in range(i + 1, len(names_noonan)):
-            sv_path = ('data/dist/analysis/inter_nn_indiv/' + names_noonan[i].split('.')[0] + '_' + 
+            sv_path = ('data/dist/analysis/inter_nn_indiv_05/' + names_noonan[i].split('.')[0] + '_' + 
                 names_noonan[j].split('.')[0] + '.png')
             merge_and_save_nn(X_noonan[i], X_noonan[j], [0.5,], sv_path)
 
